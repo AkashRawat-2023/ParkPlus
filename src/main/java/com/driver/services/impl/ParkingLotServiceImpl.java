f904152c9f33307f0ed01dac1493dd0dfcccf918
Akash Rawat <aki11.akash@gmail.com>
2023-05-02 16:17:45 +0530

    parkingLot addSpot fix

--- src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
+++ src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
@@ -2,6 +2,7 @@ package com.driver.services.impl;
 
 import com.driver.model.ParkingLot;
 import com.driver.model.Spot;
+import com.driver.model.SpotType;
 import com.driver.repository.ParkingLotRepository;
 import com.driver.repository.SpotRepository;
 import com.driver.services.ParkingLotService;
@@ -31,21 +32,26 @@ public class ParkingLotServiceImpl implements ParkingLotService {
     public Spot addSpot(int parkingLotId, Integer numberOfWheels, Integer pricePerHour) {
 
         Spot spot = new Spot();
-        if(numberOfWheels == 2) {
-            spot.setSpotType(TWO_WHEELER);
-        }else if(numberOfWheels == 4){
-            spot.setSpotType(FOUR_WHEELER);
-        }else{
-            spot.setSpotType(OTHERS);
+        if(numberOfWheels>=0 && numberOfWheels<=2) {
+            spot.setSpotType(SpotType.TWO_WHEELER);
         }
-        spot.setOccupied(false);
+        else if (numberOfWheels>=3 && numberOfWheels<=4) {
+            spot.setSpotType(SpotType.FOUR_WHEELER);
+        }
+        else{
+            spot.setSpotType(SpotType.OTHERS);
+        }
+
         spot.setPricePerHour(pricePerHour);
+        spot.setOccupied(false);
 
         ParkingLot parkingLot = parkingLotRepository1.findById(parkingLotId).get();
-        parkingLot.getSpotList().add(spot);
 
         spot.setParkingLot(parkingLot);
+        parkingLot.getSpotList().add(spot);
+
         parkingLotRepository1.save(parkingLot);
+
         return spot;
 
     }
